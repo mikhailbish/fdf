353a7d96bc538b62a579e3ba968b8d12055a4d1e
Mikhail Butuzov <mbutuzov@c1r6p6.hive.fi>
2025-01-21 11:05:00 +0200

    moved get radians outside of the rotation function

--- src/math_ops.c
+++ src/math_ops.c
@@ -6,7 +6,7 @@
 /*   By: mbutuzov <mbutuzov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 18:06:54 by mbutuzov          #+#    #+#             */
-/*   Updated: 2025/01/17 21:44:30 by mbutuzov         ###   ########.fr       */
+/*   Updated: 2025/01/21 11:02:56 by mbutuzov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,11 @@ void	rotate_x(t_3d_point *point, double angle)
 	matrix[0][1] = 0;
 	matrix[0][2] = 0;
 	matrix[1][0] = 0;
-	matrix[1][1] = cos(get_radians(angle));
-	matrix[1][2] = sin(get_radians(angle));
+	matrix[1][1] = cos(angle);
+	matrix[1][2] = sin(angle);
 	matrix[2][0] = 0;
-	matrix[2][1] = -sin(get_radians(angle));
-	matrix[2][2] = cos(get_radians(angle));
+	matrix[2][1] = -sin(angle);
+	matrix[2][2] = cos(angle);
 	mutate_3d_vector(vector, matrix);
 	point->x = vector[0];
 	point->y = vector[1];
@@ -66,15 +66,15 @@ void	rotate_y(t_3d_point *point, double angle)
 	vector[0] = (double)point->x;
 	vector[1] = (double)point->y;
 	vector[2] = (double)point->z;
-	matrix[0][0] = cos(get_radians(angle));
+	matrix[0][0] = cos(angle);
 	matrix[0][1] = 0;
-	matrix[0][2] = -sin(get_radians(angle));
+	matrix[0][2] = -sin(angle);
 	matrix[1][0] = 0;
 	matrix[1][1] = 1;
 	matrix[1][2] = 0;
-	matrix[2][0] = -sin(get_radians(angle));
+	matrix[2][0] = -sin(angle);
 	matrix[2][1] = 0;
-	matrix[2][2] = cos(get_radians(angle));
+	matrix[2][2] = cos(angle);
 	mutate_3d_vector(vector, matrix);
 	point->x = vector[0];
 	point->y = vector[1];
@@ -89,11 +89,11 @@ void	rotate_z(t_3d_point *point, double angle)
 	vector[0] = (double)point->x;
 	vector[1] = (double)point->y;
 	vector[2] = (double)point->z;
-	matrix[0][0] = cos(get_radians(angle));
-	matrix[0][1] = -sin(get_radians(angle));
+	matrix[0][0] = cos(angle);
+	matrix[0][1] = -sin(angle);
 	matrix[0][2] = 0;
-	matrix[1][0] = sin(get_radians(angle));
-	matrix[1][1] = cos(get_radians(angle));
+	matrix[1][0] = sin(angle);
+	matrix[1][1] = cos(angle);
 	matrix[1][2] = 0;
 	matrix[2][0] = 0;
 	matrix[2][1] = 0;
@@ -106,6 +106,6 @@ void	rotate_z(t_3d_point *point, double angle)
 
 void	translate_angles(t_3d_point *point)
 {
-	rotate_z(point, -45);
-	rotate_x(point, -35.264);
+	rotate_z(point, get_radians(-45));
+	rotate_x(point, get_radians(-35.264));
 }
